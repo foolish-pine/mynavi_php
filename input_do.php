a85a886575d830aabc49353a50eed15ae9a1ece7
MatsudaTakuya <matsuda.takuya41@gmail.com>
2020-09-19 16:56:09 +0900

    first commit

---
+++ input_do.php
@@ -0,0 +1,10 @@
+<?php require('dbconnect.php'); ?>
+<h2>Practice</h2>
+<pre>
+  <?php
+  $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+  $statement->execute(array($_POST['memo']));
+  echo 'メモが追加されました。';
+  ?>
+
+</pre>
